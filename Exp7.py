12aa62d46b7e641d9c51febf6be73ecda29f454b
Bodala-Gowtami <bodalagowtami19@gmail.com>
2025-07-31 16:06:37 +0530

    Performance Metrics

---
+++ Exp7.py
@@ -0,0 +1,32 @@
+import seaborn as sns
+import pandas as pd
+
+# Load dataset
+df = sns.load_dataset("iris")
+
+# View 
+print(df)
+import numpy as np
+x = df[['sepal_length', 'sepal_width']]
+y = df.petal_length
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2)
+
+from sklearn.linear_model import LinearRegression
+LR= LinearRegression()
+
+training = LR.fit(x_train, y_train)
+y_pred = LR.predict(x_test)
+print(y_pred)
+import numpy as np
+from sklearn import metrics
+
+MAE=metrics.mean_absolute_error(y_test, y_pred)
+print("Mean Absolute Error:  ",MAE)
+
+MSE=metrics.mean_squared_error(y_test, y_pred)
+print("Mean Squared Error:  ",MSE)
+
+RMSE=np.sqrt(metrics.mean_squared_error(y_test, y_pred))
+print("Root Mean Squared Error:  ",RMSE)
